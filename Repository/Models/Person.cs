218febc6f71aaeec93451150444d046fe9586a90
elibaoui otmane <elibaouiotmane@outlook.fr>
2021-02-28 22:33:26 +0100

    Move Models To Model Folder

--- Repository/Models/Person.cs
+++ Repository/Models/Person.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repository.Repositories.User
+namespace Repository.Models
 {
-    class ProfessionalExperience
+    class Person
     {
     }
 }
